89a24ae3d0cb2cbabf1f9c48e533c75c9e4c0e63
choiyui <choiyj220220@gmail.com>
2026-07-03 16:46:23 +0900

    refactor: remove unused useNavigate import in BoardList component

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { useEffect, useState, useCallback } from "react";
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 
 function Board({ data, onCheckBoxChange }) {
   return (
@@ -29,7 +29,6 @@ export default function BoardList() {
   console.log("BoardList render");
   const [list, setList] = useState([]);
   const [checkList, setCheckList] = useState([]); //기존에 체크해둔게 있다? [9,10] 중복,,?
-  let navigate = useNavigate();
 
   const getList = useCallback(() => {
     axios
