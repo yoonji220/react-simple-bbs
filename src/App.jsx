bcaf4b3b62e5ec0fc6606910b5870ea811b444e4
choiyui <choiyj220220@gmail.com>
2026-07-03 15:08:23 +0900

    feat: implement state management and navigation for Write and View components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route } from "react-router";
+import { Routes, Route, useNavigate } from "react-router";
 import BoardList from "./components/BoardList";
 import Write from "./components/Write";
 import View from "./components/View";
+import { use, useState } from "react";
 
 function App() {
+  const [boardId, setBoardId] = useState(0);
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const navigate = useNavigate();
+
+  const handleModify = _id => {
+    setBoardId(_id);
+    setIsModifyMode(true);
+    navigate("/write");
+  };
+
+  const handleCancel = () => {
+    setBoardId(0);
+    setIsModifyMode(false);
+  };
+
   return (
     <div className="container">
       <h1>React BBS</h1>
       <Routes>
         <Route path="/" element={<BoardList />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/view/:id" element={<View />} />
+        <Route
+          path="/write"
+          element={
+            <Write
+              isModifyMode={isModifyMode}
+              boardId={boardId}
+              handleCancel={handleCancel}
+            />
+          }
+        />
+        <Route
+          path="/view/:id"
+          element={<View handleModify={handleModify} />}
+        />
       </Routes>
     </div>
   );
