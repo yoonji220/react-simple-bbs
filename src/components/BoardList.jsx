74f318675f7e00eede75b2890bb54e4d64a91c56
choiyui <choiyj220220@gmail.com>
2026-07-03 11:45:30 +0900

    feat: enhance View component with error handling and dynamic content rendering

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -63,7 +63,6 @@ export default function BoardList() {
         <Link to="/write" className="btn btn-primary">
           입력
         </Link>
-        <Button variant="secondary">수정</Button>
         <Button variant="danger">삭제</Button>
       </div>
     </>
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -5,28 +5,65 @@ import { Link, useParams } from "react-router";
 
 export default function View() {
   const [content, setContent] = useState({
-    name: "",
+    writer: "",
     title: "",
     content: "",
+    date: "",
   });
+  const [isError, setIsError] = useState(false);
   const { id } = useParams();
-  useEffect(()=>{
-    
-  },[])
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/view?id=${id}`)
+      .then(response => {
+        console.log(response.data); //[{...}] 객체가 배열에 감싸져서 들어옴
+        // setContent(response.data);
+        // data가 없거나 data의 배열의 개수가 0과 같다면
+        if (!response.data || response.data.length === 0) {
+          setIsError(true);
+          return;
+        }
+        const data = response.data[0];
+        setContent({
+          writer: data.writer,
+          title: data.title,
+          content: data.content,
+          date: data.date,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        setIsError(true);
+      })
+      .finally(() => {
+        console.log("요청완료");
+      });
+  }, [id]);
+  if (isError) {
+    return (
+      <div className="text-center mt-5">
+        <h3>잘못된 접근입니다.</h3>
+        <p>다시 확인해주세요.</p>
+        <Link to="/" className="btn btn-primary mt-3">
+          홈으로 이동
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <>
-      <h2>글 제목</h2>
+      <h2>{content.title}</h2>
       <div className="d-flex justify-content-between">
-        <p>글쓴이: </p>
-        <p>날짜</p>
+        <p>글쓴이: {content.writer}</p>
+        <p>{content.date}</p>
       </div>
       <hr />
-      본문
+      {content.content}
       <hr />
       <div className="d-flex gap-1 justify-content-end">
-        <Link to="/write" className="btn btn-primary">
-          입력
+        <Link to="/" className="btn btn-primary">
+          홈
         </Link>
         <Button variant="secondary">수정</Button>
         <Button variant="danger">삭제</Button>
